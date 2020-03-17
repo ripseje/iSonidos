dbef453c0990724a10295285714dcfa503adb482
Sergio <58303460+ripseje@users.noreply.github.com>
2020-03-17 18:49:03 +0100

    Versión inicial que reproduce un solo sonido

---
+++ app/src/main/java/com/example/isonidos/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.isonidos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void reproduceSonido (View vista){
+        MediaPlayer m = new MediaPlayer();
+        int idSonido = this.getResources().getIdentifier(vista.getTag().toString(), "raw", this.getPackageName());
+        m = MediaPlayer.create(this, idSonido);
+        m.start();
+        m.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                if(mp != null){
+                    mp.release();
+                }
+            }
+        });
+    }
+}
